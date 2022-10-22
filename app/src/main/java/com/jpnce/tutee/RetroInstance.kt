d4f0370f2b670672b86c7c8f6c32be3a03ab4fdb
Abhiram <69247454+AbhiramDaita@users.noreply.github.com>
2022-10-22 13:53:26 +0530

    Update RetroInstance.kt

--- app/src/main/java/com/jpnce/tutee/RetroInstance.kt
+++ app/src/main/java/com/jpnce/tutee/RetroInstance.kt
@@ -1,1 +1,13 @@
+package com.jpnce.tutee
 
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetroInstance {
+    fun getInstance(): Retrofit {
+        val baseUrl = "https://sheets.googleapis.com"
+        println(baseUrl)
+        return Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create()).build()
+    }
+}
